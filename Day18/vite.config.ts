0c3eb8a20815d03b461844de89f30cbe6bc29a6a
Daniela Lara <laradni@icloud.com>
2023-07-29 02:35:26 -0600

    (Mod) Vite

--- Day18/vite.config.ts
+++ Day18/vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'http://lara-vel-dev.github.io/100DaysOfProjects'
 })
