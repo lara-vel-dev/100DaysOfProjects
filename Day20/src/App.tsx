159ceeb06f90c2167db4ea1fb7102a519a5ddc04
Daniela Lara <laradni@icloud.com>
2023-11-27 11:36:09 -0600

    (Add) Data + (Mod) Components

--- Day20/src/App.tsx
+++ Day20/src/App.tsx
@@ -1,5 +1,12 @@
+import "./App.css";
+import Book from "./components/Book";
+
 function App() {
-  
+  return (
+    <>
+      <Book />
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ Day20/src/components/Book.tsx
@@ -0,0 +1,9 @@
+const Book = () => {
+  return (
+    <>
+      <h1>hola</h1>
+    </>
+  );
+};
+
+export default Book;
