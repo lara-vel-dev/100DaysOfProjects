9839fee60b2dcb11b0f7f7266b0e95e6d744fb37
Daniela Lara <laradni@icloud.com>
2024-01-09 17:06:18 -0600

    (Add) Navbar styles

--- Day22/src/components/Navbar.tsx
+++ Day22/src/components/Navbar.tsx
@@ -1,7 +1,14 @@
+const styles = {
+    nav: `bg-gray-800 h-20 flex justify-between items-center p-4`,
+    heading: `text-white text-3xl`,
+}
+
 const Navbar = () => {
   return (
     <>
-      <div className="navbar">hola</div>
+      <nav className={styles.nav}>
+        <h1 className={styles.heading}>Chat App</h1>
+      </nav>
     </>
   );
 };
