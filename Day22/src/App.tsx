715187fbc113bec0dffff8c7caf6f895e2f5fb23
Daniela Lara <laradni@icloud.com>
2024-01-09 17:00:55 -0600

    (Add) Navbar component

--- Day22/src/App.tsx
+++ Day22/src/App.tsx
@@ -1,12 +1,15 @@
+import Navbar from "./components/Navbar";
+
 const style = {
-  appContainer: `max-w-[728px] mx-auto text-center`
+  appContainer: `max-w-[728px] mx-auto text-center`,
+  sectionContainter: `flex flex-col h-[90vh] bg-gray-100 mt-10 shadow-xl border relative`
 }
 
 function App() {
   return (
     <div className={style.appContainer}>
-      <section className="flex flex-col h-[90vh] bg-gray-100 mt-10 shadow-xl border relative">
-
+      <section className={style.sectionContainter}>
+        <Navbar/>
       </section>
     </div>
   );
---
+++ Day22/src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <>
+      <div className="navbar">hola</div>
+    </>
+  );
+};
+
+export default Navbar;
