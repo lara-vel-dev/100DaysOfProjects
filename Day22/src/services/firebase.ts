debcd6bd0f65531992ccd481ed23fb484678109b
Daniela Lara <laradni@icloud.com>
2024-02-06 16:43:41 -0600

    (Add) Firebase database

--- Day22/src/services/firebase.ts
+++ Day22/src/services/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -14,3 +15,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const database = getFirestore(app);
