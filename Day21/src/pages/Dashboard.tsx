50267dbf6b575c99f6869e7bb4c203d565f913c9
Daniela Lara <laradni@icloud.com>
2024-02-06 22:11:47 -0600

    (Add) SendMessage Component + (Mod) Message + Chat + App Components

--- Day21/src/pages/Dashboard.tsx
+++
@@ -1,7 +0,0 @@
-const Dashboard = () => {
-    return (<>
-        <img src="https://miro.medium.com/v2/resize:fit:1400/1*SvtF4kmd8mKZdQ6hLLMh8w.jpeg" alt="dashboard" width='100%'/>
-    </>);
-}
-
-export default Dashboard;
