0cd278e635c087925337a4bf20f8fd72e739c7a2
Daniela Lara <laradni@icloud.com>
2024-02-07 11:51:29 -0600

    (Add) Day 23

--- Day14/characters.py
+++
@@ -1,7 +0,0 @@
-characters = [
-    {'id': '1', 'name': 'Aang', 'age': 112, 'elements': 4, 'main_element': 'air'},
-    {'id': '2', 'name': 'Katara', 'age': 14, 'elements': 1, 'main_element': 'water'},
-    {'id': '3', 'name': 'Sokka', 'age': 16, 'elements': 0, 'main_element': 'none'},
-    {'id': '4', 'name': 'Toph', 'age': 12, 'elements': 1, 'main_element': 'earth'},
-    {'id': '5', 'name': 'Zuko', 'age': 16, 'elements': 1, 'main_element': 'fire'},
-]
--- Day21/src/pages/Product.tsx
+++
@@ -1,7 +0,0 @@
-const Product = () => {
-    return (<>
-        <img src="https://www.geckoboard.com/uploads/early-stage-startup-dashboard-example.png" alt="product" width='100%'/>
-    </>);
-}
-
-export default Product;
