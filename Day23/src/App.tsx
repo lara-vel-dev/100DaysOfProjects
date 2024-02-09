1f48fd7a99ed465341aeba53ae873ebf839cba2b
Daniela Lara <laradni@icloud.com>
2024-02-09 00:15:55 -0600

    (Add) Styles and logic

--- Day23/src/App.tsx
+++ Day23/src/App.tsx
@@ -1,35 +1,160 @@
-import { useState, useRef } from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [options, setOptions] = useState([{ id: 1, text: "1", colorIndex: 0 }]);
+  const [active, setActive] = useState(true);
+  const [degree, setDegree] = useState(0);
+  const [time, setTime] = useState(0);
 
-  const colors = ["#FF94F4", "#C094FF", "#94C8FF", "#2EFFDF"];
+  const options = [
+    {
+      id: 1,
+      text: "Picnin",
+      color: "#FF99F0",
+      angle: 0,
+    },
+    {
+      id: 2,
+      text: "Cineteca",
+      color: "#D899FF",
+      angle: 30,
+    },
+    {
+      id: 3,
+      text: "Cena en terraza",
+      color: "#B399FF",
+      angle: 60,
+    },
+    {
+      id: 4,
+      text: "Noche de juegos",
+      color: "#99C9FF",
+      angle: 90,
+    },
+    {
+      id: 5,
+      text: "Clase de baile",
+      color: "#FF99F0",
+      angle: 120,
+    },
+    {
+      id: 6,
+      text: "Pintar",
+      color: "#D899FF",
+      angle: 150,
+    },
+    {
+      id: 7,
+      text: "Spa",
+      color: "#B399FF",
+      angle: 180,
+    },
+    {
+      id: 8,
+      text: "Boliche",
+      color: "#99C9FF",
+      angle: 210,
+    },
+    {
+      id: 9,
+      text: "Go karts",
+      color: "#FF99F0",
+      angle: 240,
+    },
+    {
+      id: 10,
+      text: "Museo",
+      color: "#D899FF",
+      angle: 270,
+    },
+    {
+      id: 11,
+      text: "Mirador",
+      color: "#B399FF",
+      angle: 300,
+    },
+    {
+      id: 12,
+      text: "Netflix and chill",
+      color: "#99C9FF",
+      angle: 330,
+    },
+  ];
 
-  function addOptions() {
-    const nextColorIndex = options.length % colors.length;
-    const number = options.length + 1;
-    setOptions([...options, { id: number, text: number.toString(), colorIndex: nextColorIndex}]);
-  }
+  const maxSpins = 10;
+  const minSpins = 1;
 
-  function removeOption() {
-    setOptions(options.slice(0, -1));
-  }
+  const maxDegrees = 360;
+  const minDegrees = 1;
+
+  const getRandomNumber = (min, max) => {
+    return Math.round(Math.random() * (max - min) + min);
+  };
+
+  const spinWheel = () => {
+    setActive(!active);
+    const spins = getRandomNumber(minSpins, maxSpins);
+    const degrees = getRandomNumber(minDegrees, maxDegrees);
+    const fullSpins = (spins - 1) * 360;
+    const spin = fullSpins + degrees;
+    const animationTime = spins;
+    setDegree(spin);
+    setTime(animationTime);
+  };
+
+  const stopWheel = () => {
+    setDegree(0);
+    setTime(0);
+    setActive(!active); 
+  };
 
   return (
-    <>
-      <ul className="circle">
-        {options.map((item) => (
-          <li key={item.id} style={{ backgroundColor: colors[item.colorIndex] }}>
-            <div className="text" contentEditable>
-              {item.text}
+    <main>
+      <h1>Romantic Dates Roulette!</h1>
+      <section className="roulette-container">
+        <div id="selector"></div>
+        <div
+          id="roulette"
+          style={{
+            transform: `rotate(${degree}deg)`,
+            transitionDuration: `${time}s`,
+          }}
+        >
+          {options.map((item) => (
+            <div
+              key={item.id}
+              className="roulette-section"
+              style={{
+                backgroundColor: item.color,
+                transform: `rotate(${item.angle}deg) skewY(-60deg)`,
+              }}
+            >
+              <div className="roulette-section-container">
+                <p contentEditable>{item.text}</p>
+              </div>
             </div>
-          </li>
-        ))}
-      </ul>
-      <button onClick={addOptions}>Add Option</button>
-      <button onClick={removeOption}>Remove Option</button>
-    </>
+          ))}
+        </div>
+        <div className="button-container">
+          <button
+            onClick={spinWheel}
+            type="button"
+            className="btn"
+            style={active ? { display: "none" } : {}}
+          >
+            Spin!
+          </button>
+          <button
+            onClick={stopWheel}
+            type="button"
+            className="btn"
+            style={!active ? { display: "none" } : {}}
+          >
+            Reset!
+          </button>
+        </div>
+      </section>
+    </main>
   );
 }
 
