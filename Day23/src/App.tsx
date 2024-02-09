58202dcb074a6dfe3b0b05f72ee7157a9accd006
Daniela Lara <laradni@icloud.com>
2024-02-08 20:50:11 -0600

    (Add) Styles and logic

--- Day23/src/App.tsx
+++ Day23/src/App.tsx
@@ -1,9 +1,34 @@
+import { useState, useRef } from "react";
 import "./App.css";
 
 function App() {
+  const [options, setOptions] = useState([{ id: 1, text: "1", colorIndex: 0 }]);
+
+  const colors = ["#FF94F4", "#C094FF", "#94C8FF", "#2EFFDF"];
+
+  function addOptions() {
+    const nextColorIndex = options.length % colors.length;
+    const number = options.length + 1;
+    setOptions([...options, { id: number, text: number.toString(), colorIndex: nextColorIndex}]);
+  }
+
+  function removeOption() {
+    setOptions(options.slice(0, -1));
+  }
+
   return (
     <>
-      <h1>Hello world!</h1>
+      <ul className="circle">
+        {options.map((item) => (
+          <li key={item.id} style={{ backgroundColor: colors[item.colorIndex] }}>
+            <div className="text" contentEditable>
+              {item.text}
+            </div>
+          </li>
+        ))}
+      </ul>
+      <button onClick={addOptions}>Add Option</button>
+      <button onClick={removeOption}>Remove Option</button>
     </>
   );
 }
