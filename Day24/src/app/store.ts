9003a6879b110de68752aeecff9d6f0612a80a23
Daniela Lara <laradni@icloud.com>
2024-02-10 12:42:51 -0600

    (Add) Tasks and (Mod) Store

--- Day24/src/app/store.ts
+++ Day24/src/app/store.ts
@@ -1,7 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
+import taskReducer from "../features/tasks/taskSlice";
 
-const store = configureStore({
-  reducer: {},
+export const store = configureStore({
+  reducer: taskReducer,
 });
-
-export default store;
---
+++ Day24/src/features/tasks/taskSlice.ts
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+export const taskSlice = createSlice({
+  name: "tasks",
+  initialState: [],
+  reducers: {
+
+  }
+});
+
+export default taskSlice.reducer
