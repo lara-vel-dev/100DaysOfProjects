4192e575b6f86d574125aa52a88f8af5c1e9bd76
Daniela Lara <laradni@icloud.com>
2024-02-12 23:50:31 -0600

    (Add) Tailwind

--- Day24/src/App.tsx
+++ Day24/src/App.tsx
@@ -5,19 +5,18 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<ArtistsList />}></Route>
-          <Route path="/add-artist" element={<ArtistForm />}></Route>
-          <Route path="/edit-artist/:id" element={<ArtistForm />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </>
+    <div className="bg-zinc-900 h-screen text-white">
+      <div className="flex items-center justify-center h-full">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<ArtistsList />}></Route>
+            <Route path="/add-artist" element={<ArtistForm />}></Route>
+            <Route path="/edit-artist/:id" element={<ArtistForm />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </div>
   );
 }
-
-
-      
 
 export default App;
